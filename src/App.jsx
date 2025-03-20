6414e0c57e39b1f640a65401d8d322b1c4ab7303
Brandon Gonzalez <bgbeaconhous@gmail.com>
2025-03-20 00:22:25 -0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-
+import SignUpForm from './components/SignUpForm'
+import Authenticate from './components/Authenticate'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [token, setToken] = useState(null)
 
   return (
     <>
-      
+      <SignUpForm setToken={setToken} />
+      <Authenticate token={token}  />
     </>
   )
 }
